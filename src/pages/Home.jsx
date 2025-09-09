9d48648731b552dab06a33af09f7793c75d464c6
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2025-09-09 23:37:22 +0530

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,44 @@
+// import axios from "axios";
+// import { useState } from "react";
+// const Home=()=>{
+//   const [file,setFile]=useState("")
+//   const onChangeHandler=(e)=>{
+//     setFile(e.target.files[0]);
+//     console.log(file)
+//   }
+//   const handleSubmit=async()=>{
+//     let api=`https://api.cloudinary.com/v1_1/dxobvu02n/image/upload`
+//     const formData=new FormData();
+//    formData.append("file",file)
+//       formData.append("upload_preset","himanshi")
+//       formData.append("cloud_name","dxobvu02n")
+//         const response = await axios.post(api, formData);
+//         console.log(response)
+//         console.log(response.data.url);
+//   }
+//   return(
+//     <>
+//      <h3>Welcome to home page!!</h3>  
+//  Upload your images: <input type="file" onChange={onChangeHandler}/><br/><br/>
+//      <button onClick={handleSubmit}>Upload</button>
+//     </>
+//   )
+// }
+// export default Home;
+
+
 import axios from "axios";
 import { useState } from "react";
 const Home=()=>{
-  const [file,setFile]=useState("")
+  const [input,setInput]=useState({})
   const onChangeHandler=(e)=>{
-    setFile(e.target.files[0]);
-    console.log(file)
+    let name=e.target.name
+    let value=e.target.value
+    setInput(values=>({...values,[name]:value}));
+    console.log(input)
   }
   const handleSubmit=async()=>{
-    let api=`https://api.cloudinary.com/v1_1/dxobvu02n/image/upload`
-    const formData=new FormData();
-   formData.append("file",file)
-      formData.append("upload_preset","himanshi")
-      formData.append("cloud_name","dxobvu02n")
+    let api="http://localhost:8000/usersave"
         const response = await axios.post(api, formData);
         console.log(response)
         console.log(response.data.url);
@@ -19,12 +46,23 @@ const Home=()=>{
   return(
     <>
      <h3>Welcome to home page!!</h3>  
- Upload your images: <input type="file" onChange={onChangeHandler}/><br/><br/>
-     <button onClick={handleSubmit}>Upload</button>
+     enter username:<input type="text" name="username" onChange={onChangeHandler}/>
+     <br/>
+      enter email:<input type="text" name="email" onChange={onChangeHandler}/>
+     <br/>
+      enter firstname:<input type="text" name="firstname" onChange={onChangeHandler}/>
+     <br/>
+      enter lastname:<input type="text" name="lastname" onChange={onChangeHandler}/>
+     <br/>
+     <button onClick={handleSubmit}></button>
     </>
   )
 }
 export default Home;
+
+
+
+
 
 
 
